23e7e049e0e07e1a8af7cfb78a775a105bd7ec25
unnhao <unnhao@gmail.com>
2025-03-02 21:19:40 +0800

    feat: update deploy script

--- src/core/jwt.ts
+++ src/core/jwt.ts
@@ -1,13 +1,16 @@
 import jwt from "jsonwebtoken";
 import env from "../config/env";
 
+const PRIVATE_KEY = env.PRIVATE_KEY?.replace(/\\n/g, '\n').replace(/\r/g, '');
+const PUBLIC_KEY = env.PUBLIC_KEY?.replace(/\\n/g, '\n').replace(/\r/g, '');
+
 export const sign = (payload: string | Buffer | object) => {
-  return jwt.sign(payload, env.PRIVATE_KEY || "", {
+  return jwt.sign(payload, PRIVATE_KEY || "", {
     algorithm: "RS256",
     expiresIn: "1h",
   });
 };
 
 export const verify = (token: string) => {
-  return jwt.verify(token, env.PUBLIC_KEY || "", { algorithms: ["RS256"] });
+  return jwt.verify(token, PUBLIC_KEY || "", { algorithms: ["RS256"] });
 };
