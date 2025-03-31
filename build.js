f138dece8c7ceb2d94e59316b6ab83975ad95c9c
haoolii <unnhao@gmail.com>
2025-03-31 17:50:41 +0800

    feat: update buildjs

--- build.js
+++ build.js
@@ -1,13 +1,20 @@
 const esbuild = require('esbuild');
+const path = require('path');
+const fs = require('fs');
+
+// 動態取得 /src/scripts/ 資料夾下所有 .ts 文件
+const scriptFiles = fs.readdirSync('./src/scripts')
+  .filter(file => file.endsWith('.ts'))
+  .map(file => `./src/scripts/${file}`);
 
 esbuild.build({
-  entryPoints: ['src/index.ts'], // 入口文件
-  outfile: 'dist/index.js',      // 改成 index.js
+  entryPoints: ['src/index.ts', ...scriptFiles], // 入口文件
+  outdir: 'dist',                        // 改成 outdir
   bundle: true,
   platform: 'node',
   target: 'node18',
   format: 'cjs',
-  sourcemap: true,
+  sourcemap: false,
   tsconfig: './tsconfig.json',
   external: ['express'],
 }).then(() => {
